81b50de77cabe48d683292d68c0e9bb91a502d2e
Javipaur <javipaur@gmail.com>
2022-04-27 16:33:29 +0200

    Array Categorias

--- src/GifApp.js
+++ src/GifApp.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GifApp = () => {
+
+  //const categorias =['Javier','Carlos','Luis']
+
+ const [categorias, setCategorias] = useState(['Javier','Carlos','Luis'])
+
+const handleAdd= () => {
+  setCategorias([...categorias,'Pepinillos']);
+  //setCategorias(categorias =>[...categorias,'Pepe'])
+}
+
   return (
     <>
         <h2>GifApp</h2>
         <hr/>
+        <button onClick={handleAdd}>Insertar Categoria</button>
+        <ol>
+         {
+            categorias.map(categoria => {
+                return <li key={categoria}> {categoria}</li>
+            })
+         }
+         
+        </ol>
     </>
   )
 }
